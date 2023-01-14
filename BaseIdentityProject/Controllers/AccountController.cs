4c225bfeac04b97c53a65276b66278b6f3400236
oaltay <ozgenur12331@gmail.com>
2023-01-14 16:37:19 +0300

    Initial commit

---
+++ BaseIdentityProject/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using BaseIdentityProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseIdentityProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email,string password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(email);
+
+            if (hasUser==null)
+            {
+                return View();
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser,password,true,false); //3.parametre cookie'de bilgilerin tutulması, 4.paramtrede hatalı girişlerde hesap girişini bekletme
+
+            if (!signInResult.Succeeded)
+            {
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
